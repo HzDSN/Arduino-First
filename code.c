b87c75188b3d1fc8e5857367670f714f6ac0e73e
HzDSN <i@hzdsn.me>
2016-12-12 14:55:38 +0800

    HzDSN

---
+++ code.c
@@ -0,0 +1,22 @@
+#define LED 13
+#define BUTTON 12
+int val = 0;
+
+void setup()
+{
+  pinMode(LED,OUTPUT);
+  pinMode(BUTTON,INPUT);
+}
+
+void loop()
+{
+  val = digitalRead(BUTTON);
+  if(val == HIGH)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  else
+  {
+    digitalWrite(LED,LOW);
+  }
+}
